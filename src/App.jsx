8f9987896c6abbf2b4cd1f2b46b33da207af82ee
gugow06 <gustavo@phoenyx.com.br>
2023-09-04 09:54:45 -0300

    Props + Componentização.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import MeuComponente from "./components/MeuComponente";
 import Cabecalho from "./components/Cabecalho";
 import Rodape from "./components/Rodape";
 import Section from "./components/Section";
+import viteLogo from "./assets/vite.svg";
 
 export default function App(){
+
+  let viteLogoAlt = "Vite Logo";
+
   return (
     <>
       <h1>App - Componente principal!</h1>
@@ -12,7 +16,8 @@ export default function App(){
         <Cabecalho/>
         
         {/* Iniciando a área do conteúdo */}
-        <Section/>
+        <Section viteLogoProps={viteLogo} 
+        viteLogoAltProps={viteLogoAlt} />
         
         {/* Iniciando a área do rodapé */}
        <Rodape/>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,4 +1,10 @@
-export default function Section(){
+import reactlogo from "../assets/react.svg"
+
+export default function Section(props){
+
+
+    let reactlogoAlt = "React Logo"
+
     return(
         <>
         <section>
@@ -6,7 +12,8 @@ export default function Section(){
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil error libero ipsam tempora atque excepturi laudantium. Error pariatur porro mollitia, quod magni, reprehenderit laboriosam nobis nostrum soluta qui repudiandae odit!</p>
             <p>Illum id quidem ut earum fugiat, animi eius voluptate aliquam nobis minima aspernatur, aut modi dignissimos quisquam? Culpa, possimus sequi sunt, soluta architecto doloremque rem obcaecati quaerat fugit enim perferendis.</p>
             <p>Aut itaque quo vel debitis aspernatur, sint numquam officia totam pariatur ducimus laboriosam sequi, id, enim placeat at suscipit. Dolore dolorum fuga sit hic culpa quaerat rem. Magni, neque quidem.</p>
-            <img src="./assets/react.svg" alt="Logo React" />
+            <img src={reactlogo} alt={reactlogoAlt} />
+            <img src={props.viteLogoProps} alt={props.viteLogoAltProps} />
           </div>
         </section>
 
